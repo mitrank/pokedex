b349231f2ef7e5e417ece5fc7eaa124934fdba60
mitrank <mitrankshah@yahoo.com>
2026-03-27 22:32:33 +0530

    Implemented global caching

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -8,9 +8,17 @@ export const createQueryClient = () =>
   new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        // During this time (5 minutes), tRPC won't even try to re-fetch from the DB.
+        staleTime: 5 * 60 * 1000,
+
+        // This doesn't trigger reload on tab switch
+        refetchOnWindowFocus: false,
+
+        // How long to keep "unused" data in memory before clearing it
+        gcTime: 10 * 60 * 1000,
+
+        // Number of times to retry a failed fetch before showing an error
+        retry: 1,
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
